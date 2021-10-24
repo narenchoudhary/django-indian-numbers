e48764d9fefd6a22c0311d1a6528e24089a7e263
Narendra Choudhary <narenchoudhary@users.noreply.github.com>
2021-10-24 19:13:36 +0530

    Add template to reproduce issue

--- example/app/urls.py
+++ example/app/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'test/$', TemplateView.as_view(template_name='app/test.html'),
+        name='test'),
     url(r'^$', TemplateView.as_view(template_name='app/demo.html'),
-        name='demo'),
+        name='demo'),    
 ]
